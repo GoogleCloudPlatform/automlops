857689a51c2fa33187ac5a2ed2fabe6c180cf5fa
Sean Rastatter <srastatter@google.com>
2023-06-06 09:25:45 -0400

    updated nbs and new BQML example

--- examples/training/AutoMLOps/components/component_base/src/train_model.py
+++ examples/training/AutoMLOps/components/component_base/src/train_model.py
@@ -22,15 +22,11 @@ from kfp.v2.dsl import *
 from typing import *
 
 def train_model(
-    data_path: str,
-    model_directory: str
+    output_model_directory: str,
+    dataset: Input[Dataset],
+    metrics: Output[Metrics],
+    model: Output[Model]
 ):
-    """Custom component that trains a decision tree on the training data.
-
-    Args:
-        data_path: GS location where the training data.
-        model_directory: GS location of saved model.
-    """
     from sklearn.tree import DecisionTreeClassifier
     from sklearn.model_selection import train_test_split
     import pandas as pd
@@ -43,7 +39,7 @@ def train_model(
         with tf.io.gfile.GFile(uri, 'w') as f:
             pickle.dump(model, f)
 
-    df = pd.read_csv(data_path)
+    df = pd.read_csv(dataset.path)
     labels = df.pop('Class').tolist()
     data = df.values.tolist()
     x_train, x_test, y_train, y_test = train_test_split(data, labels)
@@ -51,9 +47,14 @@ def train_model(
     skmodel.fit(x_train,y_train)
     score = skmodel.score(x_test,y_test)
     print('accuracy is:',score)
+    metrics.log_metric('accuracy', (score * 100.0))
+    metrics.log_metric('framework', 'Scikit Learn')
+    metrics.log_metric('dataset_size', len(df))
 
-    output_uri = os.path.join(model_directory, f'model.pkl')
+    output_uri = os.path.join(output_model_directory, f'model.pkl')
     save_model(skmodel, output_uri)
+    model.path = output_model_directory
+
 
 def main():
     """Main executor."""
